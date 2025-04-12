2f35000ffaf5149babb265c04db92f0ce044fbeb
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2025-04-12 20:11:31 +0530

    Update page.tsx

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -47,7 +47,7 @@ export default function PlanPage() {
     setItinerary("");
     try {
       const api = process.env.NEXT_PUBLIC_API_URL;
-      const response = await fetch(`${api}`, {
+      const response = await fetch(`${api}/trips/plan/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
